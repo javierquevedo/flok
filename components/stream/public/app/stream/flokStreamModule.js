a8cff9771eca0a9a965e5a064dea812d23ec2944
Lex <lex@nothing.ch>
2014-10-26 12:37:53 +0100

    adding structure to the public part of the stream component (still empty)

---
+++ components/stream/public/app/stream/flokStreamModule.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    /**
+     * TODO
+     * @type {module}
+     */
+    var flokStreamModule = angular.module('flokStreamModule', [
+        'ngRoute',
+        'ui.bootstrap', 'ui.utils', 'pascalprecht.translate', 'onRootScope',
+        'flokFilters', 'flokDirectives', 'flokMenuModule'
+    ]);
+
+    flokStreamModule.config(['$routeProvider', 'menuServiceProvider', function($routeProvider, menuServiceProvider) {
+        $routeProvider
+            .when('/stream/:user', {
+                templateUrl: 'app/stream/stream.tpl.html',
+                controller: 'StreamCtrl',
+                controllerAs: 'stream'
+            })
+        ;
+
+        menuServiceProvider.addMenuItem(
+            {
+                url: '/stream/User',
+                name: 'Stream',
+                icon: 'tasks' // TODO what is this ?
+            }
+        );
+    }]);
+})();
+
---
+++ components/stream/public/app/stream/streamCtrl.js
@@ -0,0 +1,11 @@
+/**
+ * Controller for flok Stream
+ * @module TimeCtrl
+ */
+angular.module('flokStreamModule').controller('StreamCtrl', function() { // $scope, $timeout, $routeParams, taskProvider
+    'use strict';
+
+
+
+
+});
