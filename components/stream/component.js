25bdc2235645d7ffb41dbd8cd9ea2489c244544a
Patrick Fiaux <nodz@nothing.ch>
2014-10-26 12:27:01 +0100

    Setting up the component structure for stream

---
+++ components/stream/component.js
@@ -0,0 +1,12 @@
+var config = {
+    registerRouter: true,
+    registerLocale: true,
+    registerAngularModule: true,
+    registerPublicFiles: true,
+    jsFiles: [
+        // TODO frontend files here
+        // 'app/stream/....js',
+    ]
+};
+
+module.exports = config;
