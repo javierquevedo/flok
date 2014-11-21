36b7fb74a8b7af5b19f796f6f66cc48f0497c6d3
Patrick Fiaux <nodz@nothing.ch>
2014-11-21 16:31:38 +0100

    Adding fixture mocking the default component for testing

--- tests/public/flok/localePathConstFixture.js
+++ tests/public/flok/localePathConstFixture.js
@@ -5,4 +5,5 @@
      * Fixture for the localPathConst constant (defined in the index.ejs template)
      */
     flokModule.constant('localePathConst', '');
+    flokModule.constant('defaultComponent', '');
 })();
