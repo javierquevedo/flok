03d42f6c6c3c186948c3157d95a5406832956674
Nodz <nodz@nothing.ch>
2015-05-08 11:44:08 +0200

    Refactoring TwitterStream code into multiple smaller functions

--- components/twitter/backend/controllers/TwitterStream.js
+++ components/twitter/backend/controllers/TwitterStream.js
@@ -1,6 +1,10 @@
 /**
  * Twitter Stream Controller
  *
+ * TODO list:
+ * - Filter tweets based on config (ignore, ...)
+ * - Shutdown function
+ *
  * @copyright  Nothing Interactive 2015
  */
 'use strict';
@@ -10,23 +14,125 @@ var Twit = require('twit');
 var activeConfig = require('../../../../backend/Config.js');
 
 /**
+ * Takes a tweet object and returns an Event.
+ * TODO should this use the activity Event model & use some to json method to serialize it. code reuse would be good.
+ * TODO if we require classes from the activity components well need module dependencies and we'll need init checks that the module
+ * is enabled
+ * @param tweet
+ * @returns {{}}
+ */
+var createEventFromTweet = function(tweet) {
+    var tweetDate = new Date(Date.parse(tweet['created_at'].replace(/( \+)/, ' UTC$1')));
+
+    var event = {
+        timestamp: tweetDate.toISOString(),
+        provider: 'twitter',
+        sourceId: tweet['id_str'],
+        link: 'https://twitter.com/' + tweet.user['screen_name'] + '/status/' + tweet['id_str'],
+        title: 'tweeted',
+        message: {
+            content: '<p>' + tweet.text + '</p>',
+            format: 'html'
+        },
+        author: {
+            name: tweet.user.name
+        }
+    };
+
+    return event;
+};
+
+
+
+/**
+ * Handle a new tweet event from the stream, and post it to activities if needed.
+ * @param tweet
+ */
+var handleTweet = function(tweet) {
+
+    console.log('flok:twitter new tweet event: ', tweet);
+
+    // TODO filter if event is displayed on activity or not.
+
+    // Convert the tweet to an activity stream event
+    var event = createEventFromTweet(tweet);
+
+    //TODO extract this code to a function postEventToActivity. (note if not using http the wrapper might not event be needed).
+    var data = {
+        version: '0.0.3',
+        events: [event]
+    };
+
+    var dataString = JSON.stringify(data);
+
+    var headers = {
+        'Authorization': 'FlokApiKey ' + activeConfig.twitter.flokApiKey,
+        'Content-Type': 'application/json',
+        'Content-Length': dataString.length
+    };
+
+    var options = {
+        host: 'localhost',
+        port: 3000,
+        path: '/api/activity',
+        method: 'POST',
+        headers: headers
+    };
+
+    // Defining the http request properties
+    var req = http.request(options, function(res) {
+        res.setEncoding('utf-8');
+
+        var responseString = '';
+
+        res.on('data', function(data) {
+            responseString += data;
+        });
+
+        // TODO remove event unless needed
+        res.on('end', function() {
+            var resultObject = JSON.parse(responseString);
+        });
+    });
+
+    req.on('error', function(err) {
+        console.log('flok:twitter Twitter stream http request error: ' + err.code, err);
+    });
+
+    // Sending the tweets to the activity component
+    req.write(dataString);
+    req.end();
+};
+
+/**
+ * Initializes the twitter stream connection and setup event handlers.
+ *
  * Listen to new tweets of the Twitter account related to the keys and tokens specified in config.js
  *
- * TODO start is a good init function but it's too big and the code below should be split into helpers.
+ * TODO some event should handle the disconnection and attempt to reconnect.
  */
-exports.start = function() {
-
+var start = function() {
+    /*
+     * We use the npm module Twit to connect to the twitter stream.
+     * Here we setup the instance based on the config params.
+     */
     var T = new Twit({
-        consumer_key: activeConfig.twitter.consumerKey,
-        consumer_secret: activeConfig.twitter.consumerSecret,
-        access_token: activeConfig.twitter.accessToken,
-        access_token_secret: activeConfig.twitter.accessTokenSecret
+        'consumer_key': activeConfig.twitter.consumerKey,
+        'consumer_secret': activeConfig.twitter.consumerSecret,
+        'access_token': activeConfig.twitter.accessToken,
+        'access_token_secret': activeConfig.twitter.accessTokenSecret
     });
 
     console.log('flok:twitter Listening for tweets from @NothingAgency.');
+
+    // TODO document what this does
     var stream = T.stream('user', {});
 
-    stream.on('connect', function(request) {
+    /*
+     * Setup the event listeners on the stream:
+     * For most of them simply log
+     */
+    stream.on('connect', function(/*request*/) {
         console.log('flok:twitter Connected to Twitter API');
     });
 
@@ -43,66 +149,10 @@ exports.start = function() {
     });
 
     // Actions performed every time a new tweet is posted by the account.
-    stream.on('tweet', function(tweet) {
-        var tweetDate = new Date(Date.parse(tweet.created_at.replace(/( \+)/, ' UTC$1')));
-
-        var event = {
-            timestamp: tweetDate.toISOString(),
-            provider: 'twitter',
-            sourceId: tweet.id_str,
-            link: 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str,
-            title: 'tweeted',
-            message: {
-                content: '<p>' + tweet.text + '</p>',
-                format: 'html'
-            },
-            author: {
-                name: tweet.user.name
-            }
-        };
-
-        var data = {
-            version: '0.0.3',
-            events: [event]
-        };
-
-        var dataString = JSON.stringify(data);
-
-        var headers = {
-            'Authorization': 'FlokApiKey ' + activeConfig.twitter.flokApiKey,
-            'Content-Type': 'application/json',
-            'Content-Length': dataString.length
-        };
-
-        var options = {
-            host: 'localhost',
-            port: 3000,
-            path: '/api/activity',
-            method: 'POST',
-            headers: headers
-        };
-
-        // Defining the http request properties
-        var req = http.request(options, function(res) {
-            res.setEncoding('utf-8');
-
-            var responseString = '';
-
-            res.on('data', function(data) {
-                responseString += data;
-            });
-
-            res.on('end', function() {
-                var resultObject = JSON.parse(responseString);
-            });
-        });
-
-        req.on('error', function(err) {
-            console.log('flok:twitter Twitter stream http request error', err);
-        });
-
-        // Sending the tweets to the activity component
-        req.write(dataString);
-        req.end();
-    });
+    stream.on('tweet', handleTweet);
 };
+
+/**
+ * Expose the init function for this module.
+ */
+exports.start = start;
